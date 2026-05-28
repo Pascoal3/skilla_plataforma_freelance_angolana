a9b270b7dbe8c666a9da13b6aa69f2e9579e74b5
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-05-28 20:34:51 +0100

    Adicionando o controller do perfil

--- laravel/app/Http/Controllers/ProfileController.php
+++ laravel/app/Http/Controllers/ProfileController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ProfileService;
+use App\Models\Profile;
 use Illuminate\Http\Request;
 
-class ProfileController extends Controller
-{
-    //
+class ProfileController extends Controller {
+    protected $profileService;
+
+    public function __construct(ProfileService $profileService) {
+        $this->profileService = $profileService;
+    }
+
+    public function show($id) {
+        $profile = Profile::with('skills', 'portfolio')->findOrFail($id);
+        return response()->json($profile);
+    }
+
+    public function update(Request $request, $id) {
+        $profile = Profile::findOrFail($id);
+        $updated = $this->profileService->updateProfile($profile, $request->all());
+        return response()->json($updated);
+    }
+
+    public function updateSkills(Request $request, $id) {
+        $profile = Profile::findOrFail($id);
+        $this->profileService->syncSkills($profile, $request->skills);
+        return response()->json(['message' => 'Habilidades atualizadas!']);
+    }
 }
