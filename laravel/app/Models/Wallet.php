e39b22ec998f5d61cf68e1e3f7bebc0b644a905d
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-05-28 21:14:54 +0100

    Adicionando a model da carteira

--- laravel/app/Models/Wallet.php
+++ laravel/app/Models/Wallet.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
 
-class Wallet extends Model
-{
-    //
+class Wallet extends Model {
+    use HasUuid;
+    protected $table = 'carteiras';
+    protected $fillable = ['usuario_id', 'saldo', 'tipo', 'moeda'];
+
+    public function profile() {
+        return $this->belongsTo(Profile::class, 'usuario_id');
+    }
 }
