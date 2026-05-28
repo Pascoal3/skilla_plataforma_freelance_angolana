dc533432504556180fa77c0f6d68d93cf6abc12e
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-05-28 21:15:51 +0100

    Adicionando a model das trasações da carteira

--- laravel/app/Models/WalletTransaction.php
+++ laravel/app/Models/WalletTransaction.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
 
-class WalletTransaction extends Model
-{
-    //
+class WalletTransaction extends Model {
+    use HasUuid;
+    protected $table = 'transacoes_carteiras';
+    public $timestamps = false; // Usamos apenas criado_em
+    protected $fillable = ['carteira_origem_id', 'carteira_destino_id', 'valor', 'tipo', 'descricao', 'id_referencia', 'status'];
 }
