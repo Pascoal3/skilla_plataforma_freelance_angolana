11830e5c70f9535781b3488ea568a5ce0d6d2ad8
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-05-28 21:26:03 +0100

    Adicionando a migration dos contratos

--- laravel/database/migrations/2026_05_28_190730_create_contracts_table.php
+++ laravel/database/migrations/2026_05_28_190730_create_contracts_table.php
@@ -11,9 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contracts', function (Blueprint $table) {
-            $table->id();
+        Schema::create('contratos', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('trabalho_id')->constrained('trabalhos')->onDelete('cascade');
+            $table->foreignUuid('proposta_id')->constrained('propostas')->onDelete('cascade');
+            $table->foreignUuid('cliente_id')->constrained('perfis')->onDelete('cascade');
+            $table->foreignUuid('freelancer_id')->constrained('perfis')->onDelete('cascade');
+
+            $table->string('status_contrato')->default('ativo'); // ativo, em_disputa, concluido, cancelado
+            $table->float('valor_acordado');
+            $table->float('comissao_plataforma')->nullable();
+            $table->float('valor_freelancer')->nullable();
+    
+            $table->integer('dias_entrega');
+            $table->date('data_limite')->nullable();
+            $table->string('status_pagamento')->default('pendente'); // pendente, retido, liberado, devolvido_cliente
+    
+            $table->timestamp('trabalho_entregue_em')->nullable();
+            $table->timestamp('aprovado_em')->nullable();
             $table->timestamps();
+
+            $table->index('cliente_id');
+            $table->index('freelancer_id');
         });
     }
 
@@ -22,6 +41,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contracts');
+        Schema::dropIfExists('contratos');
     }
 };
