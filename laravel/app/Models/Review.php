c09b91e65f66af16df6977ab3a9b3bfbc877dbe6
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-05-28 21:39:08 +0100

    Adicionando a model das avaliações

--- laravel/app/Models/Review.php
+++ laravel/app/Models/Review.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Model;
 
-class Review extends Model
-{
-    //
+class Review extends Model {
+    use HasUuid;
+    protected $table = 'avaliacoes';
+    protected $fillable = ['contrato_id', 'avaliador_id', 'avaliado_id', 'nota', 'comentario'];
+
+    public function reviewer() {
+        return $this->belongsTo(Profile::class, 'avaliador_id');
+    }
+
+    public function reviewed() {
+        return $this->belongsTo(Profile::class, 'avaliado_id');
+    }
 }
