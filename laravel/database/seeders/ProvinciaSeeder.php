43bde963dabb324e8d8214ec1dfd361dfbb5119a
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-05-29 21:33:59 +0100

    Atualizando o seeder da provincia, deu bug

--- laravel/database/seeders/ProvinciaSeeder.php
+++ laravel/database/seeders/ProvinciaSeeder.php
@@ -3,31 +3,32 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Provincia; // Certifique-se que o Model Provincia existe
+use App\Models\Provincia;
+use Illuminate\Support\Str;
 
 class ProvinciaSeeder extends Seeder
 {
     public function run()
     {
         $provincias = [
-            ['nome' => 'Bengo'],
-            ['nome' => 'Benguela'],
-            ['nome' => 'Bié'],
-            ['nome' => 'Cabinda'],
-            ['nome' => 'Cunene'],
-            ['nome' => 'Huambo'],
-            ['nome' => 'Huíla'],
-            ['nome' => 'Kuando Kubango'],
-            ['nome' => 'Kwanza Norte'],
-            ['nome' => 'Kwanza Sul'],
-            ['nome' => 'Luanda'],
-            ['nome' => 'Lunda Norte'],
-            ['nome' => 'Lunda Sul'],
-            ['nome' => 'Malanje'],
-            ['nome' => 'Moxico'],
-            ['nome' => 'Namibe'],
-            ['nome' => 'Uíge'],
-            ['nome' => 'Zaire'],
+            ['nome' => 'Bengo', 'sigla' => 'BG'],
+            ['nome' => 'Benguela', 'sigla' => 'BGU'],
+            ['nome' => 'Bié', 'sigla' => 'BE'],
+            ['nome' => 'Cabinda', 'sigla' => 'CAB'],
+            ['nome' => 'Cunene', 'sigla' => 'CNN'],
+            ['nome' => 'Huambo', 'sigla' => 'HUA'],
+            ['nome' => 'Huíla', 'sigla' => 'HUI'],
+            ['nome' => 'Kuando Kubango', 'sigla' => 'KK'],
+            ['nome' => 'Kwanza Norte', 'sigla' => 'KN'],
+            ['nome' => 'Kwanza Sul', 'sigla' => 'KS'],
+            ['nome' => 'Luanda', 'sigla' => 'LUA'],
+            ['nome' => 'Lunda Norte', 'sigla' => 'LN'],
+            ['nome' => 'Lunda Sul', 'sigla' => 'LS'],
+            ['nome' => 'Malanje', 'sigla' => 'MAL'],
+            ['nome' => 'Moxico', 'sigla' => 'MOX'],
+            ['nome' => 'Namibe', 'sigla' => 'NAM'],
+            ['nome' => 'Uíge', 'sigla' => 'UIG'],
+            ['nome' => 'Zaire', 'sigla' => 'ZAI'],
         ];
 
         foreach ($provincias as $provincia) {
